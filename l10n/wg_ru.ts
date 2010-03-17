3a6818b33975ddccdc18df73c14e34fd16b12a00
pashazz <pzinin@gmail.com>
2010-03-17 22:09:39 +0300

    Translation added

---
+++ l10n/wg_ru.ts
@@ -0,0 +1,215 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>DiscDetector</name>
+    <message>
+        <location filename="../discdetector.cpp" line="11"/>
+        <source>DDT: [scan] scanning dir %1</source>
+        <translation>DDT: [cкан] сканирую папку %1</translation>
+    </message>
+    <message>
+        <location filename="../discdetector.cpp" line="16"/>
+        <source>DDT: scanning file %1</source>
+        <translation>DDT: сканирую файл %1</translation>
+    </message>
+    <message>
+        <location filename="../discdetector.cpp" line="24"/>
+        <source>DDT: [warning] unable to open file %1: error %2</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>GameDialog</name>
+    <message>
+        <location filename="../gamedialog.ui" line="14"/>
+        <source>Install Windows application from CD/DVD</source>
+        <translation>Установить Windows-приложение с CD/DVD</translation>
+    </message>
+    <message>
+        <location filename="../gamedialog.ui" line="30"/>
+        <source>Some game</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../gamedialog.ui" line="37"/>
+        <source>This game has no notes</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../gamedialog.ui" line="44"/>
+        <source>Some text</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../gamedialog.cpp" line="12"/>
+        <source>A Microsoft Windows(r) application is found on this disc. &lt;br&gt;&lt;br&gt;&lt;b&gt;%1&lt;/b&gt;&lt;br&gt;&lt;br&gt; Would you like to install it? </source>
+        <translation>На этом диске найдено приложение Microsoft Windows.&lt;br&gt;&lt;br&gt;&lt;b&gt;%1&lt;/b&gt;&lt;br&gt;&lt;br&gt;Хотите ли вы установить это приложение?</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <source>Install application</source>
+        <translation>Установить приложения Windows</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="79"/>
+        <source>Warning</source>
+        <translation>Внимание</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.cpp" line="79"/>
+        <source>Select item to run</source>
+        <translation>Выберите программу для установки</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../enginefunc.h" line="14"/>
+        <source>kdialog --passivepopup &apos;%1&apos; %2 --title &apos;%3&apos;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../enginefunc.h" line="18"/>
+        <source>notify-send &apos;%1&apos; &apos;%2&apos; -t %3</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../enginefunc.h" line="38"/>
+        <source>Downloading required components</source>
+        <translation>Загрузка необходимых компонентов</translation>
+    </message>
+    <message>
+        <location filename="../enginefunc.h" line="38"/>
+        <source>It`s near 40 MB. Please establish your Internet connection</source>
+        <translation>Это около 40 Мб. Пожалуйста, установите Интернет-соединение</translation>
+    </message>
+    <message>
+        <location filename="../enginefunc.h" line="44"/>
+        <source>engine: wine downloading finished, file %1 in directory %2</source>
+        <translation>engine: загрузка wine завершена, файл %1 в папке %2</translation>
+    </message>
+    <message>
+        <location filename="../enginefunc.h" line="54"/>
+        <source>Question</source>
+        <translation>Вопрос</translation>
+    </message>
+    <message>
+        <location filename="../enginefunc.h" line="54"/>
+        <source>I see, that Wine did installed previously for this application (Directory %1 exists).&lt;br&gt;Would you like to install wine now?</source>
+        <translation>Видимо, Wine (программа для запуска Windows-приложений) была установлена ранее для этой программы (Папка %1 cуществует)&lt;br&gt;Хотите установить Wine (снова) сейчас?</translation>
+    </message>
+    <message>
+        <location filename="../enginefunc.h" line="61"/>
+        <source>tar xvpf %1/%2 -C %3</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../enginefunc.h" line="65"/>
+        <source>engine: Wine distribution %1 unpacked to %2</source>
+        <translation>engine: Дистрибутив Wine %1 распакован в %2</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="16"/>
+        <source>This is not for Windows or Mac OSX</source>
+        <translation>Это не для винды или макоси</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="16"/>
+        <source>This program is not suitable to run on Windows or Mac OSX. &lt;br&gt; If you want to run WineGame on Win or Mac. pls remove this warning in main.cpp</source>
+        <translation>Это не для винды или макоси.  &lt;br&gt; If you want to run WineGame on Win or Mac. pls remove this warning in main.cpp</translation>
+    </message>
+</context>
+<context>
+    <name>engine</name>
+    <message>
+        <location filename="../engine.cpp" line="19"/>
+        <source>File open error</source>
+        <translation>Ошибка открытия файла</translation>
+    </message>
+    <message>
+        <location filename="../engine.cpp" line="19"/>
+        <source>Unable to open file %1. &lt;br&gt;Error: %2</source>
+        <translation>Не могу открыть файл %1.&lt;br&gt;Ошибка:%2</translation>
+    </message>
+    <message>
+        <location filename="../engine.cpp" line="36"/>
+        <source>This application has no notes</source>
+        <translation>Это приложение не имеет примечаний</translation>
+    </message>
+    <message>
+        <location filename="../engine.cpp" line="37"/>
+        <source>Please read this note about your application</source>
+        <translation>Пожалуйста, прочтите это сообщение о вашем приложении</translation>
+    </message>
+    <message>
+        <location filename="../engine.cpp" line="78"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="../engine.cpp" line="78"/>
+        <source>Wine installation not found.</source>
+        <translation>Wine не найден.</translation>
+    </message>
+    <message>
+        <location filename="../engine.cpp" line="85"/>
+        <source>engine: setting Prefix: %1</source>
+        <translation>engine: устанавливаю префикс: %1</translation>
+    </message>
+    <message>
+        <location filename="../engine.cpp" line="101"/>
+        <source>engine: preinst script returned this: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../engine.cpp" line="118"/>
+        <source>Windows  executables (*.exe)</source>
+        <translation>Исполняемые файлы Windows (*.exe)</translation>
+    </message>
+    <message>
+        <location filename="../engine.cpp" line="125"/>
+        <source>No EXE file found</source>
+        <translation>Не могу найти EXE файл</translation>
+    </message>
+    <message>
+        <location filename="../engine.cpp" line="125"/>
+        <source>Operation cancelled: No EXE file selected</source>
+        <translation>Прерываю операцию: не выбран EXE файл</translation>
+    </message>
+    <message>
+        <location filename="../engine.cpp" line="129"/>
+        <source>engine: starting Windows program %1 with wine binary %2</source>
+        <translation>engine: запускаю Windows-программу %1 с бинарником wine %2</translation>
+    </message>
+    <message>
+        <location filename="../engine.cpp" line="138"/>
+        <source>engine: postinst script returned this: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../engine.cpp" line="143"/>
+        <source>Question</source>
+        <translation>Вопрос</translation>
+    </message>
+    <message>
+        <location filename="../engine.cpp" line="143"/>
+        <source>Would you like to install a new game?</source>
+        <translation>Хотите установить новую игру (еще)?</translation>
+    </message>
+    <message>
+        <location filename="../engine.cpp" line="212"/>
+        <source>Warning</source>
+        <translation>
+Внимание</translation>
+    </message>
+    <message>
+        <location filename="../engine.cpp" line="212"/>
+        <source>Now WineGame will download required packages (from Microsoft site). Please establish your Internets</source>
+        <translation>Сейчас будут загружены необходимые компоненты (с сайта Microsoft). Пожалуйста, установите соединение с интернетом</translation>
+    </message>
+</context>
+</TS>
