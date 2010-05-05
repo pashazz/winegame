db379c63962ee7c0a7d19f0368508e2243ec3e4d
pashazz <pzinin@gmail.com>
2010-05-06 00:41:57 +0400

    Added dummy DiskDialog (will be improved in rc4)

---
+++ diskdialog.h
@@ -0,0 +1,42 @@
+/*
+    Winegame - small utility to prepare Wine and install win32 apps in it.
+    Copyright (C) 2010 Pavel Zinin <pzinin@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+*/
+
+
+#ifndef DISKDIALOG_H
+#define DISKDIALOG_H
+
+#include <QDialog>
+
+namespace Ui {
+    class DiskDialog;
+}
+
+class DiskDialog : public QDialog {
+    Q_OBJECT
+public:
+    DiskDialog(QWidget *parent = 0);
+    ~DiskDialog();
+
+protected:
+    void changeEvent(QEvent *e);
+
+private:
+    Ui::DiskDialog *ui;
+};
+
+#endif // DISKDIALOG_H
